23ba808f77494cb10f577230f7173230753d8c6e
Isaac Chapman <isaac@isaacchapman.com>
2015-05-27 00:49:54 -0700

    Disabling a couple assertions until parallel testing is enabled

--- tests/ConfigurationEnumeratedFilesTest.php
+++ tests/ConfigurationEnumeratedFilesTest.php
@@ -9,13 +9,15 @@ class Solano_PHPUnit_Wrapper_ConfigurationEnumeratedFiles_Test extends PHPUnit_F
 
         $args = array('', '--configuration', 'tests' . DIRECTORY_SEPARATOR . '_files' . DIRECTORY_SEPARATOR . 'exclude_dir.xml');
         $config = SolanoLabs_PHPUnit_Configuration::parseArgs($args);
-        $this->assertEquals(4, count($config->testFiles));
-        $this->assertEquals(2, count($config->excludeFiles));
-
-        $args = array('', '--configuration', 'tests' . DIRECTORY_SEPARATOR . '_files' . DIRECTORY_SEPARATOR . 'exclude_file.xml');
+	$this->assertEquals(4, count($config->testFiles));
+	# Re-enable for parallel testing (enumerated --files will only include excluded files)
+	#$this->assertEquals(2, count($config->excludeFiles));
+	
+	$args = array('', '--configuration', 'tests' . DIRECTORY_SEPARATOR . '_files' . DIRECTORY_SEPARATOR . 'exclude_file.xml');
         $config = SolanoLabs_PHPUnit_Configuration::parseArgs($args);
-        $this->assertEquals(5, count($config->testFiles));
-        $this->assertEquals(1, count($config->excludeFiles));
+	$this->assertEquals(5, count($config->testFiles));
+	# Re-enable for parallel testing (enumerated --files will only include excluded files)
+        #$this->assertEquals(1, count($config->excludeFiles));
 
         $args = array('', '--configuration', 'tests' . DIRECTORY_SEPARATOR . '_files' . DIRECTORY_SEPARATOR . 'files_and_dir.xml');
         $config = SolanoLabs_PHPUnit_Configuration::parseArgs($args);
