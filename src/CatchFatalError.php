a3f8df705320d3d38fc5fd202f2eb6577a66f58b
Isaac Chapman <isaac@isaacchapman.com>
2015-07-06 15:39:31 -0700

    Forgot to commit more thorough error messaging pre-merge

--- src/CatchFatalError.php
+++ src/CatchFatalError.php
@@ -7,44 +7,41 @@
  * Catch fatal errors that crash phpunit (requires PHP 5.2+)
  */
 
-
-function skippedFileDueToFatalError($errorFileName, $errorLineNumber, $errorMessage, $testFile)
+function solanoPHPUnitShutdown()
 {
-    return array(array(
-            'id' => $testFile,
-            'address' => $testFile,
-            'status' => 'error',
-            'stderr' => 'Skipped due to PHP fatal error in ' . $errorFileName . ":" . $errorLineNumber . "\n" . $errorMessage,
-            'stdout' => '',
-            'time' => 0,
-            'traceback' => array()));
-}
-
-function solanoPHPUnitShutdown() {
     $error = error_get_last();
     if ($error['type'] === E_ERROR && $outputFile = getenv('TDDIUM_OUTPUT_FILE_PROCESS')) {
-        $file = getenv('TDDIUM_LAST_TEST_FILE_STARTED');
+        $error['lastFile'] = getenv('TDDIUM_LAST_TEST_FILE_STARTED');
         $stripPath = getcwd();
-        $shortFilename = $file;
-        if (0 === strpos($file, $stripPath)) {
-            $shortFilename = substr($file, strlen($stripPath) + 1);
+        foreach ($error as $key => $value) {
+            if (0 === strpos($error[$key], $stripPath)) {
+                $error[$key] = substr($error[$key], strlen($stripPath) + 1);
+            }
         }
-        $lastFileError = array(array(
-            'id' => $shortFilename,
-            'address' => $shortFilename,
-            'status' => 'error',
-            'stderr' => 'PHP FATAL ERROR: ' . $shortFilename . ":" . $error['line'] . "\n" . $error['message'],
-            'stdout' => '',
-            'time' => 0,
-            'traceback' => array()));
+	$error['messageLine'] = $error['file'] . ' (line ' . $error['line'] . "):\n" . $error['message'];
+
         // Load outputFile
         $jsonData = SolanoLabs_PHPUnit_Util::readOutputFile($outputFile);
         foreach ($jsonData['byfile'] as $testFile => $data) {
             if (is_array($data) && !count($data)) {
-                if ($testFile == $shortFilename) {
-                    $jsonData['byfile'][$testFile] = $lastFileError;
+                if ($testFile == $error['lastFile']) {
+                    $jsonData['byfile'][$testFile] = array(array(
+                        'id' => $error['lastFile'],
+                        'address' => $error['lastFile'],
+                        'status' => 'error',
+                        'stderr' => 'PHP FATAL ERROR: ' . $error['lastFile'] . "\n" . $error['messageLine'],
+                        'stdout' => '',
+                        'time' => 0,
+                        'traceback' => array()));
                 } else {
-                    $jsonData['byfile'][$testFile] = skippedFileDueToFatalError($shortFilename, $error['line'], $error['message'], $testFile);
+                    $jsonData['byfile'][$testFile] = array(array(
+                        'id' => $testFile,
+                        'address' => $testFile,
+                        'status' => 'error',
+                        'stderr' => "Skipped due to PHP fatal error in:\n" . $error['messageLine'],
+                        'stdout' => '',
+                        'time' => 0,
+                        'traceback' => array()));
                 }
             }
         }
